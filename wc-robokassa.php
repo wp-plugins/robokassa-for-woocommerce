09336530741fa39289fd860475b4b4043ba357d2
akurganow <akurganow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-15 12:56:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/robokassa-for-woocommerce/tags/0.1.1@531365 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wc-robokassa.php
+++ wc-robokassa.php
@@ -3,8 +3,8 @@
   Plugin URI: 
   Description: Allows you to use Robokassa payment gateway with the WooCommerce plugin.
   Version: 0.1.1
-  Author: Denis Alekseev, Alexander Kurganov
-  Author URI: http://loom-studio.net/, http://akurganow.ru
+  Author: Alexander Kurganov
+  Author URI: http://polzo.ru
  */
 
 
